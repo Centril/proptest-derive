07fb1fac2d62a201c0c3db2545f9210302edfaf5
Mazdak <twingoow@gmail.com>
2017-12-22 16:23:16 +0100

    add initial custom-derive wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,37 @@
+#![recursion_limit="128"]
+
 extern crate proc_macro;
+
 extern crate syn;
-#[macro_use]
-extern crate quote;
+#[macro_use] extern crate quote;
+//#[macro_use] extern crate darling;
+
+//extern crate synstructure;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(HelloWorld)]
-pub fn hello_world(input: TokenStream) -> TokenStream {
-    // Construct a string representation of the type definition
+mod util;
+mod void;
+mod error;
+mod attr;
+mod derive;
+
+use derive::impl_proptest_arbitrary;
+
+#[proc_macro_derive(Arbitrary, attributes(proptest))]
+pub fn derive_proptest_arbitrary(input: TokenStream) -> TokenStream {
+    // Bootstrap!
+    // This function just delegates to impl_proptest_arbitrary.
+
+    // Get the concrete syntax of the type definition:
     let s = input.to_string();
-    
-    // Parse the string representation
+
+    // Parse concrete syntax into AST:
     let ast = syn::parse_derive_input(&s).unwrap();
 
-    // Build the impl
-    let gen = impl_hello_world(&ast);
-    
-    // Return the generated impl
-    gen.parse().unwrap()
-}
+    // Build impl:
+    let gen = impl_proptest_arbitrary(ast);
 
-fn impl_hello_world(ast: &syn::DeriveInput) -> quote::Tokens {
-    let name = &ast.ident;
-    quote! {
-        impl HelloWorld for #name {
-            fn hello_world() {
-                println!("Hello, World! My name is {}", stringify!(#name));
-            }
-        }
-    }
+    // Pretty print the generated tokens into the impl:
+    gen.parse().unwrap()
 }
