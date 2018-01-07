3b2a4953de717d4abe2876e9e65e335cc4ab39e7
Mazdak <twingoow@gmail.com>
2018-01-07 17:09:50 +0100

    add MIT/ASL2.0 license headers to src/

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,11 @@
+// Copyright 2018 Mazdak Farrokhzad
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! High level IR and abstract syntax tree (AST) of impls.
 //!
 //! We compile to this AST and then linearise that to Rust code.
